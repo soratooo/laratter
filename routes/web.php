514c4b5221ceaea87f1c3120ab82176072832ef5
soratooo <0116sorato@icloud.com>
2022-09-16 20:29:51 +0900

    forrow

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TweetController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\FollowController;
+
+
 Route::group(['middleware' => 'auth'], function () {
+    
+    Route::post('user/{user}/follow', [FollowController::class, 'store'])->name('follow');
+    
+    Route::post('user/{user}/unfollow', [FollowController::class, 'destroy'])->name('unfollow');
+
+    Route::post('tweet/{tweet}/favorites', [FavoriteController::class, 'store'])->name('favorites');
+    Route::post('tweet/{tweet}/unfavorites', [FavoriteController::class, 'destroy'])->name('unfavorites');
+
     Route::get('/tweet/mypage', [TweetController::class, 'mydata'])->name('tweet.mypage');
     Route::resource('tweet', TweetController::class);
 });
